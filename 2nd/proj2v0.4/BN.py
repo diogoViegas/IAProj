5bee6f8ee9c5c6143ceab35a3691e558538c7ee9
BSantosCoding <bsantospurchases@gmail.com>
2018-12-04 11:57:37 +0000

    esta quase a cena do binario

--- 2nd/proj2v0.4/BN.py
+++ 2nd/proj2v0.4/BN.py
@@ -51,7 +51,7 @@ class BN():
             for l in range(0, len(index)):
                 aux[l]=auxstr[l]
             res1+=self.computeJointProb(tuple(aux))
-            aux
+            auxstr = bin(int(auxstr) + 1)[2:].zfill(counter) #esta perto mas ainda tem 1 problema
 
 
         pass
